c1441b86deca2e73652ee9a84c0b8f27c78339c6
anurupze <109238791+anurupze@users.noreply.github.com>
2022-10-06 19:22:23 -0700

    Completing First python activity files

---
+++ Getting-Started-with-Python-AnurupP12/about_me.py
@@ -0,0 +1,11 @@
+My_Name = "Anurup Paudel"
+NickName = "Lucifer"
+Age = 22
+Used_Python_Before = True
+Hobbies = ["Sleep", "Eat", "Code", "Repeat"]
+Fav_Things= {
+  "Movie" : "The Notebook",
+  "Food" : "Chicken",
+  "Hobby": "Code"
+}
+# I believe thats all
