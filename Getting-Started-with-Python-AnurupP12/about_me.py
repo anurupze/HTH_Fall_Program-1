fbca40dab677d20db09f574e53e213562cc8007f
A-YIN--YANG <109238791+anurupze@users.noreply.github.com>
2022-11-02 13:40:26 -0700

    about_me.py

--- Getting-Started-with-Python-AnurupP12/about_me.py
+++ Getting-Started-with-Python-AnurupP12/about_me.py
@@ -2,10 +2,10 @@ My_Name = "Anurup Paudel"
 NickName = "Lucifer"
 Age = 22
 Used_Python_Before = True
-Hobbies = ["Sleep", "Eat", "Code", "Repeat"]
-Fav_Things= {
+Hobbies = ["Sleep", "Eat", "Code", "Repeat"] # List 
+# Dictionary with key:value pairs.
+Fav_Things= {                                 
   "Movie" : "The Notebook",
   "Food" : "Chicken",
   "Hobby": "Code"
 }
-# I believe thats all
