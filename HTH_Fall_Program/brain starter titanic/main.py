4a564891988a95b72720cf1f37a7bde62ec7b61a
anurupze <109238791+anurupze@users.noreply.github.com>
2022-11-16 13:35:22 -0800

    titanic visualization's

--- HTH_Fall_Program/brain starter titanic/main.py
+++ HTH_Fall_Program/brain starter titanic/main.py
@@ -36,10 +36,59 @@ visual_df = pandas.DataFrame(age_group_chart)
 seaborn.barplot(data = visual_df, x = "groups", y = "count", ci = None, palette = "magma")
 
 # Step 3: Create a line graph showing the average survival percentage of each of the previous age groups
-survival_age_pers = 
+below_16_passengers = titanic[titanic['Age'] <= 16]
+survival_16 = (len(below_16_passengers[below_16_passengers['Survived'] == 1])/ len(below_16_passengers)*100)
+youth = titanic[(titanic['Age'] > 16) & (titanic['Age'] <= 25)]
+survival_youth = (len(youth[youth['Survived'] == 1])/ len(youth)*100)
+adults = titanic[(titanic['Age'] > 25) & (titanic['Age'] <= 40)]
+survival_adults = (len(adults[adults['Survived'] == 1])/ len(adults)*100)
+mature_adults = titanic[(titanic['Age'] > 41) & (titanic['Age'] <= 59)]
+survival_mature_adults = (len(mature_adults[mature_adults['Survived'] == 1])/ len(mature_adults)*100)
+seniors = titanic[titanic['Age'] >= 60]
+survival_seniors = (len(seniors[seniors['Survived'] == 1])/ len(seniors)*100)
+
+age_group_survived = {
+    "groups": ["16 and younger", 
+               "17 to 25",
+               "26 to 40",
+               "41 to 59",
+               "60 and above"],
+    "survival":[survival_16,
+                survival_youth,
+                survival_adults,
+                survival_mature_adults,
+                survival_seniors]
+}
+visualized_df = pandas.DataFrame(age_group_survived) 
+line_graph = visualized_df.plot.line(x = "groups", y = "survival")
+
 
 # Step 4: Create a pie chart that shows the percentage of survivors that were male and the percentage of survivors that were female
-survivors = 
+males = titanic[titanic['Sex'] == 1]
+males_survived = (len(males[males['Survived'] == 1])/ len(males)*100)
+females = titanic[titanic['Sex'] == 0]
+females_survived = (len(females[females['Survived'] == 1])/ len(females)*100)
+
+survival_gender = {
+    'survival': [males_survived, females_survived],
+    'gender':[1 , 0]
+    }
+
+pie_df = pandas.DataFrame(survival_gender)
+pie_df.plot.pie(y = 'survival', autopct = '%0.1f%%', figsize = (7,7), title ='Percentage of male and female survivors')
 
 # Step 5: Create a histogram that shows the distribution of passengers between the three embarking locations: C (Cherbourg), Q (Queenstown), S (Southampton)
-titanic_description = 
+embark_num = [0,0]
+
+for index, row in titanic.iterrows():
+    if row["Embarked"] == "Q":
+        embark_num.append(1)
+    if row["Embarked"] == "C":
+        embark_num.append(2)
+    if row["Embarked"] == "S":
+        embark_num.append(3)
+
+titanic.dropna(subset=["Embarked"])
+titanic["embarked"] = embark_num
+
+titanic.hist(column="embarked")
